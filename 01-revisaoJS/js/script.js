64e0775b0255b4925456ce8b6e10180f3210ddc0
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-07 18:56:13 -0300

    map

--- 01-revisaoJS/js/script.js
+++ 01-revisaoJS/js/script.js
@@ -125,3 +125,28 @@ const pessoasNomeTerminaComA = pessoas.filter((value)=>{
 });
 
 console.log(pessoasNomeTerminaComA);
+
+
+// MAP
+
+const numbers = [5,50,80,1,2,3,4,5,8,7,11,15,22,27];
+
+const names = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47}
+];
+
+const numerosDobrados = numbers.map(function(value){
+    return value*2;
+});
+
+const justNames = names.map((obj) => {
+    return obj.nome;
+});
+
+console.log(numerosDobrados);
+console.log(justNames);
