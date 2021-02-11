f866b685abfd415e4cd8886ae18bca795653eec5
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-10 22:13:25 -0300

    manipulando prototypes

--- 01-revisaoJS/js/OOP.js
+++ 01-revisaoJS/js/OOP.js
@@ -75,3 +75,73 @@ Pessoa.prototype.estouAqui = "Estou aqui";
 
 const pessoa1 = new Pessoa('Joao', 'Vitor');
 const pessoa2 = new Pessoa('Marcos', 'Vinicius');
+
+
+/*  Manipulando prototypes   */
+
+const objA = {
+    chaveA: 'A'
+};
+
+const objB = {
+    chaveB: 'B'
+};
+
+const objC = new Object();
+objC.chaveC = 'C';
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+console.log(objB.chaveA);
+console.log(objC.chaveA);
+
+//funcao construtora
+function Product(nome, preco){
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Product.prototype.desconto = function(perc) {
+    this.preco = this.preco - (this.preco * (perc/100));
+};
+
+Product.prototype.aumento = function(perc) {
+    this.preco = this.preco + (this.preco * (perc/100));
+};
+
+const product1 = new Product('Camiseta',50);
+product1.desconto(100);
+
+console.log(product1);
+
+//produto literal
+const product2 = {
+    nome: 'Caneca',
+    preco : 15
+};
+
+Object.setPrototypeOf(product2, Product.prototype);
+
+product2.aumento(100); 
+
+console.log(product2);
+
+
+const product3 = Object.create(Product.prototype, {
+    preco : {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42
+    },
+    peso : {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 30
+    },
+});
+
+product3.aumento(30);
+
+console.log(product3);
