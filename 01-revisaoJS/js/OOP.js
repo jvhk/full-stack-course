d8981d83a5d6fa4fe5cd4c4ca48e7545a560b4ae
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-12 18:25:31 -0300

    polimorfismo

--- 01-revisaoJS/js/OOP.js
+++ 01-revisaoJS/js/OOP.js
@@ -175,3 +175,74 @@ const blusa = new Blusa('Regata', 7.5, 'Preta');
 
 console.log(caneta);
 console.log(blusa);
+
+
+/* POLIMORFISMO */
+
+//Superclasse
+function Conta(agencia, conta, saldo){
+    this.agencia = agencia;
+    this.conta = conta;
+    this.saldo = saldo;
+}
+
+Conta.prototype.sacar = function(valor){
+    if(this.saldo < valor){
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+Conta.prototype.depositar = function(valor){
+    this.saldo += valor;
+    this.verSaldo();
+};
+Conta.prototype.verSaldo = function(){
+    console.log(`Ag/C : ${this.agencia}/${this.conta} | ` + 
+    `Saldo: R$${this.saldo.toFixed(2)}`
+    );
+};
+
+const conta1 = new Conta(11, 22, 10);
+conta1.depositar(11);
+conta1.depositar(10);
+conta1.sacar(0.01);
+
+
+function ContaCorrente(agencia, conta, saldo, limite){
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+};
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function(valor){
+    if(this.saldo < valor){
+        console.log(`Saldo insuficiente: ${this.saldo}`);
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const cc = new ContaCorrente(11, 22, 0, 100);
+cc.depositar(10);
+cc.sacar(110);
+
+function ContaPoupanca(agencia, conta, saldo){
+    Conta.call(this, agencia, conta, saldo);
+};
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+console.log("Testes na Poupança:");
+
+const cp = new ContaPoupanca(12, 33, 0);
+cp.depositar(10);
+cp.sacar(110);
+cp.sacar(1);
