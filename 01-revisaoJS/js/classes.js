b10209370c7fdfcf46fa3c1d4ed1896815fc15e8
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-15 21:10:58 -0300

    getters e setters

--- 01-revisaoJS/js/classes.js
+++ 01-revisaoJS/js/classes.js
@@ -19,5 +19,60 @@ class Pessoa{
 }
 
 const pessoa1 = new Pessoa('João', 'Vitor');
-const pessoa1 = new Pessoa('Carlos', 'Vitor');
-const pessoa1 = new Pessoa('Pedro', 'Vitor');
+const pessoa2 = new Pessoa('Carlos', 'Vitor');
+const pessoa3 = new Pessoa('Pedro', 'Vitor');
+
+
+
+/*  Getters e Setters */
+console.log("---- Getters e Setters -------")
+
+const _velocidade = Symbol('velocidade')
+
+class Carro{
+    constructor(nome, velocidade){
+        this.nome = nome;
+        this[_velocidade] = 0;
+    }
+
+    set velocidade(valor){
+        console.log('SETTER');
+        if(typeof valor !== 'number'){
+            return;
+        }
+        if(valor > 100 || valor <=0){
+            return;
+        }
+        this[_velocidade] = valor;
+    }
+
+    get velocidade(){
+        console.log('GETTER');
+        return this[_velocidade];
+    }
+
+    acelerar(){
+        if(this[_velocidade] > 100){
+            return;
+        }
+        this[_velocidade]++;
+    }
+
+    freiar(){
+        if(this[_velocidade] <= 0){
+            return;
+        }
+        this[_velocidade]--;
+    }
+}
+
+const carro1 = new Carro('Fusca');
+
+/*
+for(let i = 0; i <= 200; i++){
+    carro1.acelerar();
+}
+*/
+
+carro1.velocidade = 100;   // causaria um problema sem o symbol
+console.log(carro1.velocidade);
