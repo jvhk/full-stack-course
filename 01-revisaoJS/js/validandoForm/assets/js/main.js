f6f8c61fbfcacea7b825113b1e51ac7ce14302c1
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-17 20:03:22 -0300

    promisses

--- 01-revisaoJS/js/validandoForm/assets/js/main.js
+++ 01-revisaoJS/js/validandoForm/assets/js/main.js
@@ -15,19 +15,87 @@ class ValidaFormulario {
     handleSubmit(e){
         e.preventDefault();
         const camposValidos = this.camposSaoValidos();
+        const senhasValidas = this.senhasSaoValidas();
+
+        if(camposValidos && senhasValidas){
+            alert('Formulário enviado!');
+            this.formulario.submit();
+        }
+    }
+
+    senhasSaoValidas(){
+        let valid = true;
+        const senha = this.formulario.querySelector(".senha");
+        const repetirSenha = this.formulario.querySelector(".repetir-senha"); 
+
+        if(senha.value !== repetirSenha.value){
+            valid = false;
+            this.criaErro(senha, "Campos SENHA e REPETIR SENHA precisam ser iguais");
+            this.criaErro(repetirSenha, "Campos SENHA e REPETIR SENHA precisam ser iguais");
+        }
+
+        if(senha.length < 6 || senha.length > 12){
+            valid = false;
+            this.criaErro(senha, "Senha precisa estar entre 6 e 12 caracteres");
+        }
+
+        return valid;
     }
 
     camposSaoValidos(){
         let valid = true;
 
+        for (let errorText of this.formulario.querySelectorAll(".error-text")) {
+            errorText.remove();
+        }
+
         for (let campo of this.formulario.querySelectorAll(".validar")) {
             const label = campo.previousElementSibling.innerText;
-            
+
             if(!campo.value){
                 this.criaErro(campo, `Campo ${label}  não pode estar vazio`);
                 valid = false;
             }
+
+            if(campo.classList.contains('cpf')){
+                if(this.validaCPF()){
+                    valid = false;
+                }
+            }
+
+            if(campo.classList.contains('usuario')){
+                if(this.validaUsuario()){
+                    valid = false;
+                }
+            }
         }
+        return valid;
+    }
+
+    validaCPF(campo){
+        const cpf = new ValidaCPF(campo.value);
+
+        if(!cpf.valida()){
+            this.criaErro(campo, 'CPF inválido!');
+            return false;
+        }
+        return true;
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value;
+        let valid = true;
+        
+        if(usuario.length < 3 || usuario.length > 12){
+            this.criaErro(campo, "Usuário precisa ter entre 3 e 12 caracteres");
+            valid = false;
+        }
+
+        if(usuario.match(/^[a-zA-Z0-9]+$/g)){
+            this.criaErro(campo, "Nome de usuário precisa conter apenas letras e/ou números");
+            valid = false;
+        }
+        return true;
     }
 
     criaErro(campo, msg){
