3cfb82aef2cc2bbabc7d2f1c2c89ba1cb84356fa
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-02-17 21:25:14 -0300

    métodos uteis para Promises

--- 01-revisaoJS/js/promisses.js
+++ 01-revisaoJS/js/promisses.js
@@ -23,7 +23,8 @@ esperaAi('Frase 1', rand(1,3))
     })
     .then(resposta => {
         console.log(resposta);
-        return esperaAi(333, rand(1,3));
+        //return esperaAi(333, rand(1,3));
+        return esperaAi('Frase 3', rand(1,3));
     })
     .then(resposta => {
         console.log(resposta);
@@ -32,3 +33,52 @@ esperaAi('Frase 1', rand(1,3))
         console.log('ERROR: ',e);
     });
 
+
+
+/*   MÉTODOS ÚTEIS PARA PROMISES    */
+// Promise.all Promise.race Promise.resolve Promise.reject
+
+function baixaPagina(){
+    const emCache = true;
+
+    if(emCache){
+        //return Promise.resolve('Página em cache');
+        return Promise.reject('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+
+const promises = [
+    esperaAi('Promise 1', 3000),
+    esperaAi('Promise 2', 500),
+    esperaAi('Promise 3', 1000)
+    //esperaAi(1000, 1000),
+];
+
+/*
+Promise.all(promises)
+    .then((valor) => {
+        console.log(valor);
+    })
+    .catch((e) =>{
+        console.log(e);
+    });
+*/
+Promise.race(promises)      //entrega a primeira que resolver
+    .then((valor) => {
+        console.log(valor);
+    })
+    .catch((e) =>{
+        console.log(e);
+    });
+
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => {
+        console.log("Erro: ", e);
+    })
