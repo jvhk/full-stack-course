eafa80505df75e9cf44e0b42a34ea4283b288717
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-03 23:02:34 -0300

    Escrevendo e lendo arquivos

---
+++ node2/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, 'teste.json')
+const escreve = require('./modules/escrever');
+const ler = require('./modules/ler');
+
+/*
+
+const pessoas = [
+    {nome: 'João'},
+    {nome: 'Maria'},
+    {nome: 'Carlos'},
+    {nome: 'Marcos'}
+];
+
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
+
+*/
+
+async function leArquivo(caminho) {
+    const dados = await ler(caminho);
+    renderizaDados(dados);
+}
+
+function renderizaDados(dados){
+    dados = JSON.parse(dados);
+    
+    dados.forEach(valor => console.log(valor));
+}
+
+leArquivo(caminhoArquivo);
