35e78f173a9f2b1575e69b8153d4fd84e335ecba
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-05 23:49:25 -0300

    implementando routes e controllers

--- projetos-express/server.js
+++ projetos-express/server.js
@@ -1,28 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const routes = require('./routes') 
 
 //para pegar o body no post
 app.use(express.urlencoded({extended : true}));
+app.use(routes);
 
-// Read
-app.get('/', (req, res) => {
-    res.send(`
-    <form action="/" method="POST">
-    Nome: <input type="text" name="nome">
-    <button type="submit">Enviar</button>
-    </form> 
-    `);
-});
-
-//req params
-app.get('/testes/:idUsuarios?', (req, res) => {
-  res.send(req.params.idUsuarios);
-})
-
-app.post('/', (req,res) =>{
-    console.log(req.body);
-    res.send("Recebi o formulario");
-});
-
-app.listen(port, () => console.log(`Example app listening on port port!`))
+app.listen(port, () => console.log(`Example app listening on port port!`));
