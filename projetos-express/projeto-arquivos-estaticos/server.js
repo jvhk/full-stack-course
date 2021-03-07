d38f69d398adbbb26ea467a943eaa0231bd04e4f
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-06 22:47:11 -0300

    express e webpack com probleminha no meio

---
+++ projetos-express/projeto-arquivos-estaticos/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const routes = require('./routes') 
+
+//para pegar o body no post
+app.use(express.urlencoded({extended : true}));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// views
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
