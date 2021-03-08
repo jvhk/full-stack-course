0a27a6bd6883f0d035615a14e38b3fe2df32b3da
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-07 23:44:49 -0300

    express middleware

--- projetos-express/projetoWebpackExpress/routes.js
+++ projetos-express/projetoWebpackExpress/routes.js
@@ -5,6 +5,14 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
+function meuMiddleware(req, res, next){
+    req.session = {nome:'Luiz', sobrenome: 'Miranda'};
+    console.log();
+    console.log("Passei no seu middleware");
+    console.log();
+    next();
+}
+
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost)
--- projetos-express/projetoWebpackExpress/server.js
+++ projetos-express/projetoWebpackExpress/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const routes = require('./routes') 
+const routes = require('./routes');
+
+const meuMiddleware = require('./src/middleware/middleware');
 
 //para pegar o body no post
 app.use(express.urlencoded({extended : true}));
@@ -13,6 +15,8 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
+//Meu middleware do arquivo middleware.js
+app.use(meuMiddleware);
 app.use(routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
