09d54500ccc15932da357a5bdd43af9ecf886b11
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-15 22:25:43 -0300

    Criando usuários (2)

--- projetos-express/projeto-agenda/src/models/LoginModel.js
+++ projetos-express/projeto-agenda/src/models/LoginModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
@@ -16,14 +18,25 @@ class Login {
     }
 
     async register(){
+      
       this.valida();
       if(this.errors.length > 0) return;
+      
+      await this.userExists()
+      
+      const salt = bcryptjs.genSaltSync();
+      this.body.password = bcryptjs.hashSync(this.body.password, salt); 
 
-      try{
+      try{  
         this.user = await LoginModel.create(this.body);
       }catch(e){
         console.log(e);
       }
+    }
+
+    async userExists(){
+      const user = await LoginModel.findOne({ email: this.body.email });
+      if(user) this.errors.push('Usuário já existe');
     }
 
     valida(){
