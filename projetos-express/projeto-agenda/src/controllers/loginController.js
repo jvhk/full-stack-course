5c786bdd8b065a17e649bbc0a13e614b58bd5151
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-16 22:41:56 -0300

    Logando o usuário

--- projetos-express/projeto-agenda/src/controllers/loginController.js
+++ projetos-express/projeto-agenda/src/controllers/loginController.js
@@ -1,7 +1,10 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) {
+        return res.render('login-logado');
+    }
+    return res.render('login');
 };
 
 exports.register = async (req, res) => {
@@ -13,14 +16,14 @@ exports.register = async (req, res) => {
         if(login.errors.length > 0){
             req.flash('errros', login.errors);
             req.session.save(function(){
-            return req.redirect('back');
+            return res.redirect('back');
             });
             return;
         }
 
         req.flash('success', 'Seu usuário foi criado com sucesso');
         req.session.save(function(){
-            return req.redirect('back');
+            return res.redirect('back');
         });
 
     }catch(e){
@@ -29,4 +32,38 @@ exports.register = async (req, res) => {
     }
 
     
+};
+
+exports.login = async (req, res) => {
+    try{
+        const login = new Login(req.body);
+        await login.login();
+
+        if(login.errors.length > 0){
+            req.flash('errros', login.errors);
+            req.session.save(function(){
+            return res.redirect('back');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema');
+        req.session.user = login.user;
+        req.session.save(function(){
+            return res.redirect('back');
+        });
+
+    }catch(e){
+        console.log(e);
+        return res.render('404');
+    }
+
+    
+};
+
+
+
+exports.logout = function(req, res){
+    req.session.destroy();
+    res.redirect('/');
 };
