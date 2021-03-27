837ba0af85926ab999caccc40f88f08d638fbbe1
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-03-27 19:10:24 -0300

    Rotas de alunos

--- projetos-express/api-rest/src/controllers/HomeController.js
+++ projetos-express/api-rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Joao',
-      sobrenome: 'Vitor',
-      email: 'joao@email.com',
-      idade: 20,
-      peso: 200,
-      altura: 1.9,
-    });
-    res.json(novoAluno);
+    res.json('Index da home');
   }
 }
 
