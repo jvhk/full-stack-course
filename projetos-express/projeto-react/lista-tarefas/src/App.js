91fe4dcc3b5d5358cce9ef4a7a1b8d98bd7ac881
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-02 21:06:48 -0300

    Componentes

--- projetos-express/projeto-react/lista-tarefas/src/App.js
+++ projetos-express/projeto-react/lista-tarefas/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Main from './components/Main';
 import './App.css';
 
-//Primeiro componente
-export default function App(){
-  return <h1>Hello World</h1>;
+// Primeiro componente
+export default function App() {
+  return <Main />;
 }
