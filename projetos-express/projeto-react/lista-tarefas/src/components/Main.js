fb5673b0b64c49f0e4c4691a886fbcd7981fdafc
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-04 22:52:31 -0300

    Adicionando tarefas

--- projetos-express/projeto-react/lista-tarefas/src/components/Main.js
+++ projetos-express/projeto-react/lista-tarefas/src/components/Main.js
@@ -11,11 +11,25 @@ export default class Main extends Component {
     state = {
       novaTarefa: '',
       tarefas: [
-        'Fazer café',
-        'Beber água',
-        'Estudar',
+
       ],
     };
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      const { tarefas } = this.state;
+      let { novaTarefa } = this.state;
+      novaTarefa = novaTarefa.trim();
+
+      if (tarefas.indexOf(novaTarefa) !== -1) {
+        return;
+      }
+
+      const novasTarefas = [...tarefas];
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+      });
+    }
 
     handleChange = (e) => {
       this.setState({
@@ -29,7 +43,7 @@ export default class Main extends Component {
         <div className="main">
           <h1>Lista de tarefas</h1>
 
-          <form action="#">
+          <form onSubmit={this.handleSubmit} action="#" className="form">
             <input
               onChange={this.handleChange}
               type="text"
@@ -44,10 +58,10 @@ export default class Main extends Component {
             {tarefas.map((tarefa) => (
               <li key={tarefa}>
                 {tarefa}
-                <div>
+                <span>
                   <FaEdit className="edit" />
                   <FaWindowClose className="delete" />
-                </div>
+                </span>
               </li>
             ))}
           </ul>
