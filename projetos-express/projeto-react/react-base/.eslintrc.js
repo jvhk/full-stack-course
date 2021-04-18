a49a58cda495ac4f6b08e83fa85c4216b7b3ceb2
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-18 18:53:46 -0300

    limpando projeto

--- projetos-express/projeto-react/react-base/.eslintrc.js
+++ projetos-express/projeto-react/react-base/.eslintrc.js
@@ -1,15 +1,16 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'prettier/react'
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
-  perser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -17,16 +18,14 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks',
-  ],
+  plugins: ['react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps':'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
   },
 };
