ef7f82bd0734b048fc533a02590d4b08dc8c8a62
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-20 01:29:09 -0300

    react router dom

--- projetos-express/projeto-react/react-base/src/components/Header/index.js
+++ projetos-express/projeto-react/react-base/src/components/Header/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import { Nav } from './styled';
 
 export default function Header() {
   return (
     <Nav>
-      <a>
+      <Link to="/">
         <FaHome size={24} />
-      </a>
-      <a>
+      </Link>
+      <Link to="/login">
         <FaUserAlt size={24} />
-      </a>
-      <a>
+      </Link>
+      <Link to="/asdasdsa">
         <FaSignInAlt size={24} />
-      </a>
+      </Link>
     </Nav>
   );
 }
