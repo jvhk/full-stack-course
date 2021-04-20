c706731a32fd0d66cd058bed082df784e963bcd1
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-19 22:01:18 -0300

    Menu da aplicação

--- projetos-express/projeto-react/react-base/src/App.js
+++ projetos-express/projeto-react/react-base/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/Login';
 import GlobalStyles from './styles/globalStyles';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
