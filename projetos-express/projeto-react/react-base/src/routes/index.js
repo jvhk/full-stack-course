231af4565740a4feb199b9f9d81271dc283f4fac
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-27 22:33:23 -0300

    React Toastify

--- projetos-express/projeto-react/react-base/src/routes/index.js
+++ projetos-express/projeto-react/react-base/src/routes/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import MyRoute from './MyRoute';
 import Login from '../pages/Login/index';
 import Page404 from '../pages/Page404/index';
 
 export default function Routes() {
+  toast.success('Oi, sucesso');
+  toast.error('Oi, ERRO!');
   return (
     <Switch>
       <MyRoute exact path="/" component={Login} />
