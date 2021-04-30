088ea6f4cd482ea0d919d0b12129b82a43bad1b3
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-04-29 22:42:23 -0300

    Redux + React states e actions

--- projetos-express/projeto-react/react-base/src/pages/Login/index.js
+++ projetos-express/projeto-react/react-base/src/pages/Login/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Container } from '../../styles/globalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
   return (
     <Container>
       <Title>
@@ -10,7 +21,9 @@ export default function Login() {
         <small>Ola</small>
       </Title>
       <Paragrafo>lorem ipsum</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
