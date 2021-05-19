faf10bde0be61b2799f5b792ba349c44ad88d210
jvhk <joaovitoroliveiracamara@hotmail.com>
2021-05-19 00:01:13 -0300

    Exibindo a foto do aluno

--- projetos-express/projeto-react/api-consumer/src/pages/Aluno/index.js
+++ projetos-express/projeto-react/api-consumer/src/pages/Aluno/index.js
@@ -4,23 +4,28 @@ import PropTypes from 'prop-types';
 import { isEmail, isInt, isFloat } from 'validator';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
+import {FaEdit, FaUserCircle} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 import { Container } from '../../styles/globalStyles';
-import { Form } from './styled';
+import { Form, ProfilePicture,Title } from './styled';
 import Loading from '../../components/Loading';
 import axios from '../../services/axios';
 import history from '../../services/history';
 import * as actions from '../../store/modules/auth/actions';
 
+
 export default function Aluno({ match }) {
   const dispatch = useDispatch();
-  const id = get(match, 'params.id', 0);
+
+  const id = get(match, 'params.id', '');
   const [nome, setNome] = useState('');
   const [sobreNome, setSobrenome] = useState('');
   const [email, setEmail] = useState('');
   const [idade, setIdade] = useState('');
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
+  const [foto, setFoto] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -31,6 +36,8 @@ export default function Aluno({ match }) {
         setIsLoading(true);
         const { data } = await axios.get(`alunos/${id}`);
         const Foto = get(data, 'Fotos[0].url', '');
+
+        setFoto(Foto);
 
         setNome(data.nome);
         setSobrenome(data.sobreNome);
@@ -141,7 +148,16 @@ export default function Aluno({ match }) {
     <Container>
       <Loading isLoading={isLoading} />
 
-      <h1>{id ? 'Editar aluno' : 'Novo aluno'}</h1>
+      <Title>{id ? 'Editar aluno' : 'Novo aluno'}</Title>
+
+      {id && (
+        <ProfilePicture>
+          {foto ? (<img src={foto} alt={nome}/>) : (<FaUserCircle size={180} />}
+          <Link to={`/fotos/${id}`}>
+            <FaEdit size={24} />
+          </Link>
+        </ProfilePicture>
+      )}
 
       <Form onSubmit={handleSubmit}>
         <input
